cadddc50774f5d40fea09710ff885c09e6447951
DinoDani30 <124879246+DinoDani30@users.noreply.github.com>
2024-12-15 18:38:38 +0100

    Pdfs y ini periodo

---
+++ Sprin boot/src/main/java/com/tfg/eldest/pdfs/PdfController.java
@@ -0,0 +1,112 @@
+package com.tfg.eldest.pdfs;
+
+import com.tfg.eldest.actividadformacion.ActividadFormacion;
+import com.tfg.eldest.usuario.Usuario;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(path = "pdfs")
+public class PdfController {
+    @Value("${api.url}")  // Cargar la URL desde application.properties
+    private String apiUrl;
+
+    private final PdfService pdfService;
+
+    public PdfController(PdfService pdfService) {
+        this.pdfService = pdfService;
+    }
+
+    private ActividadFormacion obtenerActividadFormacion(String tipo, Long id) {
+        // Crear un objeto RestTemplate para hacer la llamada a la API
+        RestTemplate restTemplate = new RestTemplate();
+
+        System.out.println(tipo + ", " + id);
+
+        String apiUrl = this.apiUrl;
+        if (tipo.equals("Actividad")) {
+            // URL de la API que devuelve las actividades
+            apiUrl = this.apiUrl + "/actividades/" + id;
+
+        } else {
+            // URL de la API que devuelve las actividades
+            apiUrl = this.apiUrl + "/formaciones/" + id;
+        }
+
+        // Realizar la solicitud GET a la API de actividades
+        ResponseEntity<ActividadFormacion> response = restTemplate.exchange(
+                apiUrl,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<ActividadFormacion>() {
+                }
+        );
+
+        // Obtener la lista de actividades de la respuesta
+        ActividadFormacion actividadFormacion = response.getBody();
+
+        return actividadFormacion;
+    }
+
+    @GetMapping("generar/{tipo}/{id}")
+    public void generatePdf(@PathVariable String tipo, @PathVariable Long id, HttpServletResponse response) throws IOException {
+        // Obtener la actividad de formación
+        ActividadFormacion actividad = obtenerActividadFormacion(tipo, id);
+
+        // Formatear los encargados y participantes como una cadena HTML
+        String encargadosHtml = actividad.getEncargados().stream()
+                .map(encargado -> "<li>" + encargado.getId() + "</li>")
+                .collect(Collectors.joining());
+
+        String participantesHtml = actividad.getParticipantes().stream()
+                .map(participante -> "<li>" + participante.getId() + "</li>")
+                .collect(Collectors.joining());
+
+        // Define el path a la plantilla HTML
+        String templatePath = "src/main/resources/templates/pdfs/PdfActividadFormacion.html";
+
+        // Mapear los valores dinámicos para la plantilla
+        Map<String, String> placeholders = new HashMap<>();
+        placeholders.put("titulo", actividad.getTitulo());
+        placeholders.put("tipo", actividad.getTipo());
+        placeholders.put("numero", actividad.getNumero());
+        placeholders.put("fech_realiz", String.valueOf(actividad.getFech_realiz()));
+        placeholders.put("encargados", encargadosHtml);  // Se pasa la lista formateada
+        placeholders.put("participantes", participantesHtml);  // Se pasa la lista formateada
+        placeholders.put("grupo_edad", actividad.getGrupo_edad());
+        placeholders.put("duracion", String.valueOf(actividad.getDuracion()));
+        placeholders.put("objetivos", actividad.getObjetivos());
+        placeholders.put("materiales", actividad.getMateriales());
+        placeholders.put("descripcion", actividad.getDescripcion());
+        placeholders.put("observaciones", actividad.getObservaciones());
+        placeholders.put("numVoluntarios", String.valueOf(actividad.getNumVoluntarios()));
+        placeholders.put("numParticipantes", String.valueOf(actividad.getNumParticipantes()));
+        placeholders.put("visible", String.valueOf(actividad.getVisible()));
+        placeholders.put("periodo", actividad.getPeriodo().getNombre());
+
+        // Genera el PDF como un array de bytes
+        byte[] pdfContents = pdfService.generatePdf(templatePath, placeholders);
+
+        // Configura los encabezados de la respuesta para que el PDF se muestre correctamente
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "inline; filename=" + tipo + "_" + id + ".pdf");
+
+        // Escribe el contenido PDF en la salida
+        response.getOutputStream().write(pdfContents);
+        response.getOutputStream().flush();
+    }
+
+
+}
--- Sprin boot/src/main/java/com/tfg/eldest/periodo/PeriodoConfig.java
+++ Sprin boot/src/main/java/com/tfg/eldest/periodo/PeriodoConfig.java
@@ -1,10 +1,5 @@
 package com.tfg.eldest.periodo;
 
-import com.tfg.eldest.actividadformacion.ActividadFormacion;
-import com.tfg.eldest.actividadformacion.actividad.ActividadRepository;
-import com.tfg.eldest.actividadformacion.formacion.FormacionRepository;
-import com.tfg.eldest.usuario.Usuario;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,49 +9,37 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.time.Month.*;
+import static java.time.Month.JUNE;
+import static java.time.Month.SEPTEMBER;
 
 @Configuration
 public class PeriodoConfig {
-    private final ActividadRepository actividadRepository;
-    private final FormacionRepository formacionRepository;
-
-    @Autowired
-    public PeriodoConfig(ActividadRepository actividadRepository, FormacionRepository formacionRepository) {
-        this.actividadRepository = actividadRepository;
-        this.formacionRepository = formacionRepository;
-    }
-
-    private List<ActividadFormacion> recuperarActividades() {
-        return actividadRepository.findAll();
-    }
-
-    private List<ActividadFormacion> recuperarFormaciones() {
-        return formacionRepository.findAll();
-    }
-
-    private List<ActividadFormacion> recuperarActividadesFormaciones() {
-        List<ActividadFormacion> actividadesFormaciones = new ArrayList<>();
-        actividadesFormaciones.addAll(recuperarActividades());
-        actividadesFormaciones.addAll(recuperarFormaciones());
-
-        return actividadesFormaciones;
-    }
-
     @Bean
     @Order(2)
     CommandLineRunner commandLineRunnerPeriodo(PeriodoRepository repository) {
         return args -> {
-            Periodo per1 = new Periodo(1L, "curso 22/23", LocalDate.of(2022, SEPTEMBER, 4),
-                    LocalDate.of(2023, JUNE, 24));
+            List<Periodo> periodos = new ArrayList<>();
 
-            Periodo per2 = new Periodo(2L, "curso 23/24", LocalDate.of(2023, SEPTEMBER, 5),
-                    LocalDate.of(2024, JUNE, 25));
 
-            Periodo per3 = new Periodo(3L, "curso 24/25", LocalDate.of(2024, SEPTEMBER, 6),
-                    LocalDate.of(2025, JUNE, 26));
 
-            repository.saveAll(List.of(per1, per2, per3));
+            // Creamos 20 periodos
+            for (long i = 1; i <= 5; i++) {
+                int year = 2002 + (int) i;
+
+                // Creamos el periodo
+                Periodo periodo = new Periodo(
+                        i,
+                        "curso " + year + "/" + (year+1),
+                        LocalDate.of(year, SEPTEMBER, ((3+(int) i)%30)+1),
+                        LocalDate.of(year+1, JUNE, ((23+(int) i)%30)+1)
+                );
+
+                periodos.add(periodo);
+            }
+
+            System.out.println(periodos);
+            // Guardamos todos los periodos en el repositorio
+            repository.saveAll(periodos);
         };
     }
 }
