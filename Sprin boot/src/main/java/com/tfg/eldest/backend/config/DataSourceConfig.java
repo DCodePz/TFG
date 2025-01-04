0cf31612088e069cac6c22538eda5bf0c759edc8
DinoDani30 <124879246+DinoDani30@users.noreply.github.com>
2025-01-04 23:04:16 +0100

    Upload de ficheros por la reestructuración

--- Sprin boot/src/main/java/com/tfg/eldest/backend/config/DataSourceConfig.java
+++ Sprin boot/src/main/java/com/tfg/eldest/backend/config/DataSourceConfig.java
@@ -1,11 +1,10 @@
-package com.tfg.eldest.config;
+package com.tfg.eldest.backend.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.web.client.RestTemplate;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
